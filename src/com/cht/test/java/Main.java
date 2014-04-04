37b29882b844f093c916e2c9d03c8f9186685699
gtcht <chthq@126.com>
2014-04-04 15:28:38 +0800

    cht 1

---
+++ src/com/cht/test/java/Main.java
@@ -0,0 +1,14 @@
+package com.cht.test.java;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is test 2!");
+	}
+
+}
